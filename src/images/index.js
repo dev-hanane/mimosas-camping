a852ad6570ea19a57c43d105e72abc2df08e8251
CHAAOUAN HANANE <Chaaouanhanane18@gmail.com>
2026-07-23 22:40:15 +0100

    Add files via upload

---
+++ src/images/index.js
@@ -0,0 +1,37 @@
+// Logo
+export { default as logo } from './logo.png'
+
+// Images générales
+export { default as activ } from './activ.png'
+
+// Verts (green spaces)
+export { default as vertsImage } from './verts/image.png'
+export { default as vertsBack2 } from './verts/back2.png'
+export { default as vertsImg1 } from './verts/img1.png'
+export { default as vertsImg2 } from './verts/img2.png'
+export { default as vertsImg3 } from './verts/img3.png'
+
+// Chalets
+export { default as chaletsImg } from './chalets/chalets.png'
+export { default as chaletsSection2 } from './chalets/section2.png'
+export { default as chaletsImg1 } from './chalets/img1.png'
+export { default as chaletsImg2 } from './chalets/img2.png'
+export { default as chaletsImg3 } from './chalets/img3.png'
+
+// Maisons
+export { default as maisonsImg1 } from './maisons/img1.png'
+export { default as maisonsImg2 } from './maisons/img2.png'
+export { default as maisonsImg3 } from './maisons/img3.png'
+
+// Espaces
+export { default as espacesTente } from './espaces/tente.png'
+export { default as espacesZoneCar } from './espaces/zone_car.png'
+
+// Restauration
+export { default as restaurationImg1 } from './restauration/img1.png'
+export { default as restaurationImg2 } from './restauration/img2.png'
+
+// Social
+export { default as facebook } from './facebook.png'
+export { default as instagram } from './instagram.png'
+export { default as telephone } from './telephone.png'
